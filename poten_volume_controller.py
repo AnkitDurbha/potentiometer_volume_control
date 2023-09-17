7022f013f348e97f7e1b9406645a4642bde9ced7
Ankit Durbha <ankitdurbha@gmail.com>
2023-09-16 20:03:14 -0400

    potentiometer code

--- poten_volume_controller.py
+++ poten_volume_controller.py
@@ -1,41 +1,35 @@
-
 from pynput.keyboard import Key,Controller
-import time
 from ADCDevice import *
 
 adc = ADCDevice() # Define an ADCDevice class object
 
 def setup():
     global adc
-    if(adc.detectI2C(0x48)): # Detect the pcf8591.
-        adc = PCF8591()
-    elif(adc.detectI2C(0x4b)): # Detect the ads7830
+    if(adc.detectI2C(0x4b)): # Detect the ADC
         adc = ADS7830()
     else:
         print("No correct I2C address found, \n"
-        "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
         "Program Exit. \n")
         exit(-1)
         
-def loop():
+def main():
     lastValue = 0
     keyboard = Controller()
     value = 1
     while True:
-        value = adc.analogRead(0)    # read the ADC value of channel 0
-        if value > 200:
+        value = adc.analogRead(0)    # read the ADC value
+        if value > 200:   # Rounding off the value
             value = 200
-        
         value /= 20
         round(value, 0)
         value = int(value)
-        if value > lastValue:    
+        if value > lastValue:    # Simulating key presses, depending on which direction the potentiometer has been moved
             for i in range(value - lastValue):
                 keyboard.press(Key.media_volume_down)
-        else:
+        if lastValue > value:
             for i in range(lastValue - value):
                 keyboard.press(Key.media_volume_up)
-        lastValue = value
+        lastValue = value     # Setting lastValue = to value so we know what direction the potentiometer was turned on the last iteration of the loop
         
 
 def destroy():
@@ -45,6 +39,6 @@ if __name__ == '__main__':   # Program entrance
     print ('Program is starting ... ')
     try:
         setup()
-        loop()
+        main()
     except KeyboardInterrupt: # Press ctrl-c to end the program.
         destroy()
